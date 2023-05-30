d6b1196e009142196c225bd8651ba8435abae05b
Robert Konrad <git2016@robdangero.us>
2023-05-30 14:30:00 +0200

    Start emitting opcodes

--- Sources/kong.c
+++ Sources/kong.c
@@ -4,29 +4,116 @@
 #include "tokenizer.h"
 
 #include <assert.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 const char *filename = "in/test.kong";
+
+typedef struct opcode {
+	enum { OPCODE_VAR, OPCODE_NOT } type;
+
+	union {
+		struct {
+			int a;
+		} var;
+	};
+} opcode;
+
+typedef struct opcodes {
+	opcode o[256];
+	size_t size;
+} opcodes;
+
+opcodes all_opcodes;
+
+void emit_op(opcode opcode) {
+	all_opcodes.o[all_opcodes.size] = opcode;
+	all_opcodes.size += 1;
+}
+
+void emit_expression(expression *expression) {
+	switch (expression->type) {
+	case EXPRESSION_BINARY: {
+		switch (expression->binary.op) {
+		case OPERATOR_EQUALS:
+		case OPERATOR_NOT_EQUALS:
+		case OPERATOR_GREATER:
+		case OPERATOR_GREATER_EQUAL:
+		case OPERATOR_LESS:
+		case OPERATOR_LESS_EQUAL:
+		case OPERATOR_MINUS:
+		case OPERATOR_PLUS:
+		case OPERATOR_DIVIDE:
+		case OPERATOR_MULTIPLY:
+		case OPERATOR_NOT:
+		case OPERATOR_OR:
+		case OPERATOR_AND:
+		case OPERATOR_MOD:
+		case OPERATOR_ASSIGN:
+			break;
+		}
+		break;
+	}
+	case EXPRESSION_UNARY:
+		switch (expression->unary.op) {
+		case OPERATOR_EQUALS:
+		case OPERATOR_NOT_EQUALS:
+		case OPERATOR_GREATER:
+		case OPERATOR_GREATER_EQUAL:
+		case OPERATOR_LESS:
+		case OPERATOR_LESS_EQUAL:
+		case OPERATOR_MINUS:
+		case OPERATOR_PLUS:
+		case OPERATOR_DIVIDE:
+		case OPERATOR_MULTIPLY:
+		case OPERATOR_NOT: {
+			emit_expression(expression->unary.right);
+			opcode opcode;
+			opcode.type = OPCODE_NOT;
+			emit_op(opcode);
+			break;
+		}
+		case OPERATOR_OR:
+		case OPERATOR_AND:
+		case OPERATOR_MOD:
+		case OPERATOR_ASSIGN:
+			break;
+		}
+	case EXPRESSION_BOOLEAN:
+	case EXPRESSION_NUMBER:
+	case EXPRESSION_STRING:
+	case EXPRESSION_VARIABLE:
+	case EXPRESSION_GROUPING:
+	case EXPRESSION_CALL:
+	case EXPRESSION_MEMBER:
+	case EXPRESSION_CONSTRUCTOR:
+		break;
+	}
+}
+
+void emit_statement(statement *statement) {
+	switch (statement->type) {
+	case STATEMENT_EXPRESSION:
+		emit_expression(statement->expression);
+		break;
+	case STATEMENT_RETURN_EXPRESSION:
+		break;
+	case STATEMENT_IF:
+		break;
+	case STATEMENT_BLOCK:
+		break;
+	case STATEMENT_LOCAL_VARIABLE:
+		break;
+	}
+}
 
 void convert_function_block(struct statement *block) {
 	if (block->type != STATEMENT_BLOCK) {
 		error("Expected a block", 0, 0);
 	}
 	for (size_t i = 0; i < block->block.statements.size; ++i) {
-		statement *s = block->block.statements.s[i];
-		switch (s->type) {
-		case STATEMENT_EXPRESSION:
-			break;
-		case STATEMENT_RETURN_EXPRESSION:
-			break;
-		case STATEMENT_IF:
-			break;
-		case STATEMENT_BLOCK:
-			break;
-		case STATEMENT_LOCAL_VARIABLE:
-			break;
-		}
+		emit_statement(block->block.statements.s[i]);
 	}
 }
 
