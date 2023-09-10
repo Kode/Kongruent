8278adcf22d2f0a83e280fe768dfa6cab92c4ba5
Robert Konrad <git2016@robdangero.us>
2023-09-11 00:39:11 +0200

    Assign registers for globals

--- Sources/backends/hlsl.c
+++ Sources/backends/hlsl.c
@@ -12,6 +12,7 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 static char *type_string(type_id type) {
 	if (type == float_id) {
@@ -301,6 +302,8 @@ static void write_types(char *hlsl, size_t *offset, shader_stage stage, type_id 
 	}
 }
 
+static int global_register_indices[512];
+
 static void write_globals(char *hlsl, size_t *offset, function *main) {
 	global_id globals[256];
 	size_t globals_size = 0;
@@ -308,14 +311,16 @@ static void write_globals(char *hlsl, size_t *offset, function *main) {
 
 	for (size_t i = 0; i < globals_size; ++i) {
 		global g = get_global(globals[i]);
+		int register_index = global_register_indices[globals[i]];
+
 		if (g.type == sampler_type_id) {
-			*offset += sprintf(&hlsl[*offset], "SamplerState _%" PRIu64 " : register(s0);\n\n", g.var_index);
+			*offset += sprintf(&hlsl[*offset], "SamplerState _%" PRIu64 " : register(s%i);\n\n", g.var_index, register_index);
 		}
 		else if (g.type == tex2d_type_id) {
-			*offset += sprintf(&hlsl[*offset], "Texture2D<float4> _%" PRIu64 " : register(t0);\n\n", g.var_index);
+			*offset += sprintf(&hlsl[*offset], "Texture2D<float4> _%" PRIu64 " : register(t%i);\n\n", g.var_index, register_index);
 		}
 		else {
-			*offset += sprintf(&hlsl[*offset], "cbuffer _%" PRIu64 " {\n", g.var_index);
+			*offset += sprintf(&hlsl[*offset], "cbuffer _%" PRIu64 " : register(b%i) {\n", g.var_index, register_index);
 			type *t = get_type(g.type);
 			for (size_t i = 0; i < t->members.size; ++i) {
 				*offset += sprintf(&hlsl[*offset], "\t%s _%" PRIu64 "_%s;\n", get_name(get_type(t->members.m[i].type.type)->name), g.var_index,
@@ -536,6 +541,28 @@ static void hlsl_export_fragment(char *directory, function *main) {
 }
 
 void hlsl_export(char *directory) {
+	int cbuffer_index = 0;
+	int texture_index = 0;
+	int sampler_index = 0;
+
+	memset(global_register_indices, 0, sizeof(global_register_indices));
+
+	for (global_id i = 0; get_global(i).type != NO_TYPE; ++i) {
+		global g = get_global(i);
+		if (g.type == sampler_type_id) {
+			global_register_indices[i] = sampler_index;
+			sampler_index += 1;
+		}
+		else if (g.type == tex2d_type_id) {
+			global_register_indices[i] = texture_index;
+			texture_index += 1;
+		}
+		else {
+			global_register_indices[i] = cbuffer_index;
+			cbuffer_index += 1;
+		}
+	}
+
 	function *vertex_shaders[256];
 	size_t vertex_shaders_size = 0;
 
