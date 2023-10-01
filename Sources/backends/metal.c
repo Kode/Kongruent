ed1d4b51899273ad808f8f3171b3958c0eb5f485
Robert Konrad <git2016@robdangero.us>
2023-10-02 01:31:53 +0200

    Write everything into one Metal file

--- Sources/backends/metal.c
+++ Sources/backends/metal.c
@@ -38,65 +38,14 @@ static char *function_string(name_id func) {
 	return get_name(func);
 }
 
-static void write_bytecode(char *metal, char *directory, const char *filename, const char *name, uint8_t *output, size_t output_size) {
+static void write_code(char *metal, char *directory, const char *filename, const char *name) {
 	char full_filename[512];
 
 	{
-		sprintf(full_filename, "%s/%s.h", directory, filename);
-		FILE *file = fopen(full_filename, "wb");
-		fprintf(file, "#include <stdint.h>\n\n");
-		fprintf(file, "extern uint8_t *%s;\n", name);
-		fprintf(file, "extern size_t %s_size;\n", name);
-		fclose(file);
-	}
-
-	{
-		sprintf(full_filename, "%s/%s.c", directory, filename);
+		sprintf(full_filename, "%s/%s.metal", directory, filename);
 
 		FILE *file = fopen(full_filename, "wb");
-		fprintf(file, "#include \"%s.h\"\n\n", filename);
-
-		fprintf(file, "uint8_t *%s = \"", name);
-		for (size_t i = 0; i < output_size; ++i) {
-			// based on the encoding described in https://github.com/adobe/bin2c
-			if (output[i] == '!' || output[i] == '#' || (output[i] >= '%' && output[i] <= '>') || (output[i] >= 'A' && output[i] <= '[') ||
-			    (output[i] >= ']' && output[i] <= '~')) {
-				fprintf(file, "%c", output[i]);
-			}
-			else if (output[i] == '\a') {
-				fprintf(file, "\\a");
-			}
-			else if (output[i] == '\b') {
-				fprintf(file, "\\b");
-			}
-			else if (output[i] == '\t') {
-				fprintf(file, "\\t");
-			}
-			else if (output[i] == '\v') {
-				fprintf(file, "\\v");
-			}
-			else if (output[i] == '\f') {
-				fprintf(file, "\\f");
-			}
-			else if (output[i] == '\r') {
-				fprintf(file, "\\r");
-			}
-			else if (output[i] == '\"') {
-				fprintf(file, "\\\"");
-			}
-			else if (output[i] == '\\') {
-				fprintf(file, "\\\\");
-			}
-			else {
-				fprintf(file, "\\%03o", output[i]);
-			}
-		}
-		fprintf(file, "\";\n");
-
-		fprintf(file, "size_t %s_size = %" PRIu64 ";\n\n", name, output_size);
-
-		fprintf(file, "/*\n%s*/\n", metal);
-
+		fprintf(file, "%s", metal);
 		fclose(file);
 	}
 }
@@ -259,41 +208,49 @@ static void find_referenced_globals(function *f, global_id *globals, size_t *glo
 	}
 }
 
-static void write_types(char *metal, size_t *offset, shader_stage stage, type_id input, type_id output, function *main) {
-	type_id types[256];
-	size_t types_size = 0;
-	find_referenced_types(main, types, &types_size);
+static type_id vertex_inputs[256];
+static size_t vertex_inputs_size = 0;
+static type_id fragment_inputs[256];
+static size_t fragment_inputs_size = 0;
 
-	for (size_t i = 0; i < types_size; ++i) {
-		type *t = get_type(types[i]);
+static bool is_vertex_input(type_id t) {
+	for (size_t i = 0; i < vertex_inputs_size; ++i) {
+		if (t == vertex_inputs[i]) {
+			return true;
+		}
+	}
+	return false;
+}
+
+static bool is_fragment_input(type_id t) {
+	for (size_t i = 0; i < fragment_inputs_size; ++i) {
+		if (t == fragment_inputs[i]) {
+			return true;
+		}
+	}
+	return false;
+}
+
+static void write_types(char *metal, size_t *offset) {
+	for (type_id i = 0; get_type(i) != NULL; ++i) {
+		type *t = get_type(i);
 
 		if (!t->built_in && t->attribute != add_name("pipe")) {
 			*offset += sprintf(&metal[*offset], "struct %s {\n", get_name(t->name));
 
-			if (stage == SHADER_STAGE_VERTEX && types[i] == input) {
+			if (is_vertex_input(i)) {
 				for (size_t j = 0; j < t->members.size; ++j) {
-					*offset +=
-					    sprintf(&metal[*offset], "\t%s %s : TEXCOORD%" PRIu64 ";\n", type_string(t->members.m[j].type.type), get_name(t->members.m[j].name), j);
+					*offset += sprintf(&metal[*offset], "\t%s %s [[attribute(%" PRIu64 ")]];\n", type_string(t->members.m[j].type.type),
+					                   get_name(t->members.m[j].name), j);
 				}
 			}
-			else if (stage == SHADER_STAGE_VERTEX && types[i] == output) {
+			else if (is_fragment_input(i)) {
 				for (size_t j = 0; j < t->members.size; ++j) {
 					if (j == 0) {
-						*offset += sprintf(&metal[*offset], "\t%s %s : SV_POSITION;\n", type_string(t->members.m[j].type.type), get_name(t->members.m[j].name));
+						*offset += sprintf(&metal[*offset], "\t%s %s [[position]];\n", type_string(t->members.m[j].type.type), get_name(t->members.m[j].name));
 					}
 					else {
-						*offset += sprintf(&metal[*offset], "\t%s %s : TEXCOORD%" PRIu64 ";\n", type_string(t->members.m[j].type.type),
-						                   get_name(t->members.m[j].name), j - 1);
-					}
-				}
-			}
-			else if (stage == SHADER_STAGE_FRAGMENT && types[i] == input) {
-				for (size_t j = 0; j < t->members.size; ++j) {
-					if (j == 0) {
-						*offset += sprintf(&metal[*offset], "\t%s %s : SV_POSITION;\n", type_string(t->members.m[j].type.type), get_name(t->members.m[j].name));
-					}
-					else {
-						*offset += sprintf(&metal[*offset], "\t%s %s : TEXCOORD%" PRIu64 ";\n", type_string(t->members.m[j].type.type),
+						*offset += sprintf(&metal[*offset], "\t%s %s [[user(locn%" PRIu64 ")]];\n", type_string(t->members.m[j].type.type),
 						                   get_name(t->members.m[j].name), j - 1);
 					}
 				}
@@ -310,14 +267,10 @@ static void write_types(char *metal, size_t *offset, shader_stage stage, type_id
 
 static int global_register_indices[512];
 
-static void write_globals(char *metal, size_t *offset, function *main) {
-	global_id globals[256];
-	size_t globals_size = 0;
-	find_referenced_globals(main, globals, &globals_size);
-
-	for (size_t i = 0; i < globals_size; ++i) {
-		global g = get_global(globals[i]);
-		int register_index = global_register_indices[globals[i]];
+static void write_globals(char *metal, size_t *offset) {
+	for (global_id i = 0; get_global(i).type != NO_TYPE; ++i) {
+		global g = get_global(i);
+		int register_index = global_register_indices[i];
 
 		if (g.type == sampler_type_id) {
 			*offset += sprintf(&metal[*offset], "SamplerState _%" PRIu64 " : register(s%i);\n\n", g.var_index, register_index);
@@ -340,19 +293,36 @@ static void write_globals(char *metal, size_t *offset, function *main) {
 	}
 }
 
-static void write_functions(char *metal, size_t *offset, shader_stage stage, function *main) {
-	function *functions[256];
-	size_t functions_size = 0;
+static function_id vertex_functions[256];
+static size_t vertex_functions_size = 0;
+static function_id fragment_functions[256];
+static size_t fragment_functions_size = 0;
 
-	functions[functions_size] = main;
-	functions_size += 1;
+static bool is_vertex_function(function_id f) {
+	for (size_t i = 0; i < vertex_functions_size; ++i) {
+		if (f == vertex_functions[i]) {
+			return true;
+		}
+	}
+	return false;
+}
 
-	find_referenced_functions(main, functions, &functions_size);
+static bool is_fragment_function(function_id f) {
+	for (size_t i = 0; i < fragment_functions_size; ++i) {
+		if (f == fragment_functions[i]) {
+			return true;
+		}
+	}
+	return false;
+}
 
-	for (size_t i = 0; i < functions_size; ++i) {
-		function *f = functions[i];
+static void write_functions(char *metal, size_t *offset) {
+	for (function_id i = 0; get_function(i) != NULL; ++i) {
+		function *f = get_function(i);
 
-		assert(f->block != NULL);
+		if (f->block == NULL) {
+			continue;
+		}
 
 		uint8_t *data = f->code.o;
 		size_t size = f->code.size;
@@ -366,29 +336,26 @@ static void write_functions(char *metal, size_t *offset, shader_stage stage, fun
 		}
 
 		assert(parameter_id != 0);
-		if (f == main) {
-			if (stage == SHADER_STAGE_VERTEX) {
-				*offset += sprintf(&metal[*offset], "%s main(%s _%" PRIu64 ") {\n", type_string(f->return_type.type), type_string(f->parameter_type.type),
-				                   parameter_id);
-			}
-			else if (stage == SHADER_STAGE_FRAGMENT) {
-				if (f->return_type.array_size > 0) {
-					*offset += sprintf(&metal[*offset], "struct _render_targets {\n");
-					for (uint32_t j = 0; j < f->return_type.array_size; ++j) {
-						*offset += sprintf(&metal[*offset], "\t%s _%i : SV_Target%i;\n", type_string(f->return_type.type), j, j);
-					}
-					*offset += sprintf(&metal[*offset], "};\n\n");
-					*offset += sprintf(&metal[*offset], "_render_targets main(%s _%" PRIu64 ") {\n", type_string(f->parameter_type.type), parameter_id);
+
+		if (is_vertex_function(i)) {
+			*offset +=
+			    sprintf(&metal[*offset], "%s main(%s _%" PRIu64 ") {\n", type_string(f->return_type.type), type_string(f->parameter_type.type), parameter_id);
+		}
+		else if (is_fragment_function(i)) {
+			if (f->return_type.array_size > 0) {
+				*offset += sprintf(&metal[*offset], "struct _render_targets {\n");
+				for (uint32_t j = 0; j < f->return_type.array_size; ++j) {
+					*offset += sprintf(&metal[*offset], "\t%s _%i : SV_Target%i;\n", type_string(f->return_type.type), j, j);
 				}
-				else {
-					*offset += sprintf(&metal[*offset], "%s main(%s _%" PRIu64 ") : SV_Target0 {\n", type_string(f->return_type.type),
-					                   type_string(f->parameter_type.type), parameter_id);
-				}
+				*offset += sprintf(&metal[*offset], "};\n\n");
+				*offset += sprintf(&metal[*offset], "_render_targets main(%s _%" PRIu64 ") {\n", type_string(f->parameter_type.type), parameter_id);
 			}
 			else {
-				assert(false);
+				*offset += sprintf(&metal[*offset], "%s main(%s _%" PRIu64 ") : SV_Target0 {\n", type_string(f->return_type.type),
+				                   type_string(f->parameter_type.type), parameter_id);
 			}
 		}
+
 		else {
 			*offset += sprintf(&metal[*offset], "%s %s(%s _%" PRIu64 ") {\n", type_string(f->return_type.type), get_name(f->name),
 			                   type_string(f->parameter_type.type), parameter_id);
@@ -425,7 +392,7 @@ static void write_functions(char *metal, size_t *offset, shader_stage stage, fun
 			}
 			case OPCODE_RETURN: {
 				if (o->size > offsetof(opcode, op_return)) {
-					if (f == main && stage == SHADER_STAGE_FRAGMENT && f->return_type.array_size > 0) {
+					if (is_fragment_function(i) && f->return_type.array_size > 0) {
 						*offset += sprintf(&metal[*offset], "\t{\n");
 						*offset += sprintf(&metal[*offset], "\t\t_render_targets rts;\n");
 						for (uint32_t j = 0; j < f->return_type.array_size; ++j) {
@@ -466,66 +433,23 @@ static void write_functions(char *metal, size_t *offset, shader_stage stage, fun
 	}
 }
 
-static void metal_export_vertex(char *directory, function *main) {
+static void metal_export_everything(char *directory) {
 	char *metal = (char *)calloc(1024 * 1024, 1);
 	size_t offset = 0;
 
-	type_id vertex_input = main->parameter_type.type;
-	type_id vertex_output = main->return_type.type;
+	write_types(metal, &offset);
 
-	assert(vertex_input != NO_TYPE);
-	assert(vertex_output != NO_TYPE);
+	write_globals(metal, &offset);
 
-	write_types(metal, &offset, SHADER_STAGE_VERTEX, vertex_input, vertex_output, main);
-
-	write_globals(metal, &offset, main);
-
-	write_functions(metal, &offset, SHADER_STAGE_VERTEX, main);
-
-	char *output;
-	size_t output_size;
-	int result = compile_hlsl_to_d3d11(metal, &output, &output_size, SHADER_STAGE_VERTEX, false);
-	assert(result == 0);
-
-	char *name = get_name(main->name);
+	write_functions(metal, &offset);
 
 	char filename[512];
-	sprintf(filename, "kong_%s", name);
+	sprintf(filename, "kong_metal");
 
 	char var_name[256];
-	sprintf(var_name, "%s_code", name);
+	sprintf(var_name, "metal_code");
 
-	write_bytecode(metal, directory, filename, var_name, output, output_size);
-}
-
-static void metal_export_fragment(char *directory, function *main) {
-	char *metal = (char *)calloc(1024 * 1024, 1);
-	size_t offset = 0;
-
-	type_id pixel_input = main->parameter_type.type;
-
-	assert(pixel_input != NO_TYPE);
-
-	write_types(metal, &offset, SHADER_STAGE_FRAGMENT, pixel_input, NO_TYPE, main);
-
-	write_globals(metal, &offset, main);
-
-	write_functions(metal, &offset, SHADER_STAGE_FRAGMENT, main);
-
-	uint8_t *output;
-	size_t output_size;
-	int result = compile_hlsl_to_d3d11(metal, &output, &output_size, SHADER_STAGE_FRAGMENT, false);
-	assert(result == 0);
-
-	char *name = get_name(main->name);
-
-	char filename[512];
-	sprintf(filename, "kong_%s", name);
-
-	char var_name[256];
-	sprintf(var_name, "%s_code", name);
-
-	write_bytecode(metal, directory, filename, var_name, output, output_size);
+	write_code(metal, directory, filename, var_name);
 }
 
 void metal_export(char *directory) {
@@ -551,12 +475,6 @@ void metal_export(char *directory) {
 		}
 	}
 
-	function *vertex_shaders[256];
-	size_t vertex_shaders_size = 0;
-
-	function *fragment_shaders[256];
-	size_t fragment_shaders_size = 0;
-
 	for (type_id i = 0; get_type(i) != NULL; ++i) {
 		type *t = get_type(i);
 		if (!t->built_in && t->attribute == add_name("pipe")) {
@@ -578,22 +496,22 @@ void metal_export(char *directory) {
 			for (function_id i = 0; get_function(i) != NULL; ++i) {
 				function *f = get_function(i);
 				if (f->name == vertex_shader_name) {
-					vertex_shaders[vertex_shaders_size] = f;
-					vertex_shaders_size += 1;
+					vertex_functions[vertex_functions_size] = i;
+					vertex_functions_size += 1;
+
+					vertex_inputs[vertex_inputs_size] = f->parameter_type.type;
+					vertex_inputs_size += 1;
 				}
 				else if (f->name == fragment_shader_name) {
-					fragment_shaders[fragment_shaders_size] = f;
-					fragment_shaders_size += 1;
+					fragment_functions[fragment_functions_size] = i;
+					fragment_functions_size += 1;
+
+					fragment_inputs[fragment_inputs_size] = f->parameter_type.type;
+					fragment_inputs_size += 1;
 				}
 			}
 		}
 	}
 
-	for (size_t i = 0; i < vertex_shaders_size; ++i) {
-		metal_export_vertex(directory, vertex_shaders[i]);
-	}
-
-	for (size_t i = 0; i < fragment_shaders_size; ++i) {
-		metal_export_fragment(directory, fragment_shaders[i]);
-	}
+	metal_export_everything(directory);
 }
