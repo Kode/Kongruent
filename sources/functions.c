41717a97c3ab3a8d69127eaad79e31a0cdc4ea4b
Robert Konrad <git2016@robdangero.us>
2025-03-22 21:29:39 +0100

    Fix the typo

--- sources/functions.c
+++ sources/functions.c
@@ -634,7 +634,7 @@ void functions_init(void) {
 	add_func_float3_float3_float3("reflect");
 	add_func_uint("primitive_index");
 	add_func_float3_float3("abs");
-	add_func_float3_float3_float3("dot");
+	add_func_float_float3_float3("dot");
 	add_func_float3_float3_float3("cross");
 	add_func_float3_float3("saturate3");
 	add_func_float_float("saturate");
