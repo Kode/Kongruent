d0bfa0470d2b5ba8702bd58e7e559cb11d9fba91
luboslenco <lubos.lenco@gmail.com>
2025-03-23 08:43:28 +0100

    Add step and smoothstep functions

--- sources/functions.c
+++ sources/functions.c
@@ -634,6 +634,8 @@ void functions_init(void) {
 	add_func_float3_float3_float3("reflect");
 	add_func_uint("primitive_index");
 	add_func_float3_float3("abs");
+	add_func_float_float_float("step");
+	add_func_float_float_float("smoothstep");
 	add_func_float_float3_float3("dot");
 	add_func_float3_float3_float3("cross");
 	add_func_float3_float3("saturate3");
