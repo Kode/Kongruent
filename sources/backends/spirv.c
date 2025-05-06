12b8738cd9a03bf1d2308a686a8c90084b3d4e2d
luboslenco <lubos.lenco@gmail.com>
2025-05-06 18:03:15 +0200

    Cleanup

--- sources/backends/spirv.c
+++ sources/backends/spirv.c
@@ -1863,62 +1863,48 @@ static void write_function(instructions_buffer *instructions, function *f, spirv
 			write_op_store(instructions, convert_kong_index_to_spirv_id(o->op_store_var.to.index), convert_kong_index_to_spirv_id(o->op_store_var.from.index));
 			break;
 		}
-		case OPCODE_ADD_AND_STORE_VARIABLE: {
-			spirv_id to = convert_kong_index_to_spirv_id(o->op_store_var.to.index);
-			spirv_id from = convert_kong_index_to_spirv_id(o->op_store_var.from.index);
-
-			spirv_id to_loaded = write_op_load(instructions, convert_type_to_spirv_id(o->op_store_var.to.type.type), to);
-			spirv_id from_loaded = write_op_load(instructions, convert_type_to_spirv_id(o->op_store_var.from.type.type), from);
-
-			if (vector_base_type(o->op_store_var.to.type.type) == float_id) {
-				spirv_id result = write_op_f_add(instructions, convert_type_to_spirv_id(o->op_store_var.to.type.type), to_loaded, from_loaded);
-				write_op_store(instructions, to, result);
-			}
-			else if (vector_base_type(o->op_store_var.to.type.type) == int_id || vector_base_type(o->op_store_var.to.type.type) == uint_id) {
-				spirv_id result = write_op_i_add(instructions, convert_type_to_spirv_id(o->op_store_var.to.type.type), to_loaded, from_loaded);
-				write_op_store(instructions, to, result);
-			}
-
-			break;
-		}
-		case OPCODE_SUB_AND_STORE_VARIABLE: {
-			spirv_id to = convert_kong_index_to_spirv_id(o->op_store_var.to.index);
-			spirv_id from = convert_kong_index_to_spirv_id(o->op_store_var.from.index);
-
-			spirv_id to_loaded = write_op_load(instructions, convert_type_to_spirv_id(o->op_store_var.to.type.type), to);
-			spirv_id from_loaded = write_op_load(instructions, convert_type_to_spirv_id(o->op_store_var.from.type.type), from);
-
-			if (vector_base_type(o->op_store_var.to.type.type) == float_id) {
-				spirv_id result = write_op_f_sub(instructions, convert_type_to_spirv_id(o->op_store_var.to.type.type), to_loaded, from_loaded);
-				write_op_store(instructions, to, result);
-			}
-			else if (vector_base_type(o->op_store_var.to.type.type) == int_id || vector_base_type(o->op_store_var.to.type.type) == uint_id) {
-				spirv_id result = write_op_i_sub(instructions, convert_type_to_spirv_id(o->op_store_var.to.type.type), to_loaded, from_loaded);
-				write_op_store(instructions, to, result);
-			}
-
-			break;
-		}
-		case OPCODE_MULTIPLY_AND_STORE_VARIABLE: {
-			spirv_id to = convert_kong_index_to_spirv_id(o->op_store_var.to.index);
-			spirv_id from = convert_kong_index_to_spirv_id(o->op_store_var.from.index);
-
-			spirv_id to_loaded = write_op_load(instructions, convert_type_to_spirv_id(o->op_store_var.to.type.type), to);
-			spirv_id from_loaded = write_op_load(instructions, convert_type_to_spirv_id(o->op_store_var.from.type.type), from);
-
-			spirv_id result = write_op_f_mul(instructions, convert_type_to_spirv_id(o->op_store_var.to.type.type), to_loaded, from_loaded);
-			write_op_store(instructions, to, result);
-
-			break;
-		}
+		case OPCODE_ADD_AND_STORE_VARIABLE:
+		case OPCODE_SUB_AND_STORE_VARIABLE:
+		case OPCODE_MULTIPLY_AND_STORE_VARIABLE:
 		case OPCODE_DIVIDE_AND_STORE_VARIABLE: {
 			spirv_id to = convert_kong_index_to_spirv_id(o->op_store_var.to.index);
 			spirv_id from = convert_kong_index_to_spirv_id(o->op_store_var.from.index);
-
 			spirv_id to_loaded = write_op_load(instructions, convert_type_to_spirv_id(o->op_store_var.to.type.type), to);
 			spirv_id from_loaded = write_op_load(instructions, convert_type_to_spirv_id(o->op_store_var.from.type.type), from);
+			spirv_id result;
 
-			spirv_id result = write_op_f_div(instructions, convert_type_to_spirv_id(o->op_store_var.to.type.type), to_loaded, from_loaded);
+			switch (o->type) {
+			case OPCODE_ADD_AND_STORE_VARIABLE: {
+				if (vector_base_type(o->op_store_var.to.type.type) == float_id) {
+					result = write_op_f_add(instructions, convert_type_to_spirv_id(o->op_store_var.to.type.type), to_loaded, from_loaded);
+				}
+				else if (vector_base_type(o->op_store_var.to.type.type) == int_id || vector_base_type(o->op_store_var.to.type.type) == uint_id) {
+					result = write_op_i_add(instructions, convert_type_to_spirv_id(o->op_store_var.to.type.type), to_loaded, from_loaded);
+				}
+				break;
+			}
+			case OPCODE_SUB_AND_STORE_VARIABLE: {
+				if (vector_base_type(o->op_store_var.to.type.type) == float_id) {
+					result = write_op_f_sub(instructions, convert_type_to_spirv_id(o->op_store_var.to.type.type), to_loaded, from_loaded);
+				}
+				else if (vector_base_type(o->op_store_var.to.type.type) == int_id || vector_base_type(o->op_store_var.to.type.type) == uint_id) {
+					result = write_op_i_sub(instructions, convert_type_to_spirv_id(o->op_store_var.to.type.type), to_loaded, from_loaded);
+				}
+				break;
+			}
+			case OPCODE_MULTIPLY_AND_STORE_VARIABLE: {
+				result = write_op_f_mul(instructions, convert_type_to_spirv_id(o->op_store_var.to.type.type), to_loaded, from_loaded);
+				break;
+			}
+			case OPCODE_DIVIDE_AND_STORE_VARIABLE: {
+				result = write_op_f_div(instructions, convert_type_to_spirv_id(o->op_store_var.to.type.type), to_loaded, from_loaded);
+				break;
+			}
+			default:
+				assert(false);
+				break;
+			}
+
 			write_op_store(instructions, to, result);
 
 			break;
