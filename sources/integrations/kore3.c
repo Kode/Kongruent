403ad96681a46334cddd5bf79880ac27fccc51c3
Robert Konrad <git2016@robdangero.us>
2025-05-25 14:21:04 +0200

    [Vulkan] Set cube textures

--- sources/integrations/kore3.c
+++ sources/integrations/kore3.c
@@ -1798,7 +1798,7 @@ void kore3_export(char *directory, api_kind api) {
 							debug_context context = {0};
 							error(context, "Cube maps can not be writable");
 						}
-						fprintf(output, "\tkore_%s_descriptor_set_set_texture_cube_view_srv(device, &set->set, &parameters->%s, %zu);\n", api_short,
+						fprintf(output, "\tkore_vulkan_descriptor_set_set_sampled_cube_image_descriptor(device, &set->set, &parameters->%s, %zu);\n", 
 						        get_name(g->name), other_index);
 
 						fprintf(output, "\tset->%s = parameters->%s;\n", get_name(g->name), get_name(g->name));
